fac027bb10659cd4d295ab72bd479023714f02b7
5gbus <bintang55okewin@gmail.com>
2024-10-23 04:01:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// RTP animation
-document.addEventListener("DOMContentLoaded", function () {
-    const rtpBars = document.querySelectorAll('.rtp-bar');
-
-    rtpBars.forEach(bar => {
-        const rtpPercentage = bar.getAttribute('data-rtp');
-        bar.style.width = rtpPercentage + '%';
-    });
-});
-
 // Fungsi untuk navigasi
 function navigateTo(page) {
     window.location.href = page; // Ganti halaman dengan URL yang diberikan
