dc2c25fc13cf939a043e3066d7c3cbfe5df2eb38
5gbus <bintang55okewin@gmail.com>
2024-10-23 03:43:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ document.addEventListener("DOMContentLoaded", function () {
 function navigateTo(sectionId) {
     document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
 }
+
+// RTP animation
+document.addEventListener("DOMContentLoaded", function () {
+    const rtpBars = document.querySelectorAll('.rtp-bar');
+
+    rtpBars.forEach(bar => {
+        // Menghasilkan RTP acak antara 80 hingga 100
+        const rtpPercentage = Math.floor(Math.random() * (100 - 80 + 1)) + 80;
+        bar.style.width = rtpPercentage + '%';
+        bar.setAttribute('data-rtp', rtpPercentage); // Update data-rtp attribute
+    });
+});
